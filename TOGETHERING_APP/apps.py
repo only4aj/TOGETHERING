ca78d863f6c16ad35a032a63b5aefd3e63ec5e5c
only4aj <onlyforanshjuneja@gmail.com>
2024-01-17 16:25:21 +0530

    1st Commit

---
+++ TOGETHERING_APP/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TogetheringAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TOGETHERING_APP'
---
+++ TOGETHERING_APP/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render,redirect, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Hello, Welcome to my New Project!")

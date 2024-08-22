4f7ae0a78cd1927acba20c2fd7ce782d2b775fb3
only4aj <onlyforanshjuneja@gmail.com>
2024-08-22 12:16:40 +0530

    routing file updated

--- TOGETHERING_APP/routing.py
+++ TOGETHERING_APP/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .consumer import Chatting
 
 websocket_urlpatterns = [
-    re_path(r"ws/watch/(?P<room_name>\w+)/$", Chatting.as_asgi()),
+    re_path(r"wss/watch/(?P<room_name>\w+)/$", Chatting.as_asgi()),
 ]

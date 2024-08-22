bf6b3d635ae8102ba4091d660ebb03f7f05c02a6
only4aj <onlyforanshjuneja@gmail.com>
2024-08-22 11:26:54 +0530

    whitenoise

--- TOGETHERING/settings.py
+++ TOGETHERING/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,3 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR , 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build' , 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
